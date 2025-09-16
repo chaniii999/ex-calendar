561eaed9c7418e56feca20b3b09fa3e35e4211c5
chaniii999 <psc0729@naver.com>
2025-09-16 13:21:57 +0900

    refactor: enhance User entity with access modifiers and update timestamps

--- src/main/java/com/calendar/entity/User.java
+++ src/main/java/com/calendar/entity/User.java
@@ -1,36 +1,43 @@
 package com.calendar.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.Builder;
-import lombok.Getter;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 @Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 @Builder
 @Getter
 public class User {
 
     @Id
     @Column(length = 26, nullable = false, updatable = false)
-    String id;
+    private String id;
 
     @Column(length = 50, nullable = false, unique = true)
-    String email;
+    private String email;
 
+    @Setter
     @Column(length = 50, nullable = false)
-    String password;
+    private String password;
 
+    @Setter
     @Column(length = 20, nullable = false)
-    String nickname;
+    private String nickname;
 
     @Column(nullable = false)
     private ZonedDateTime createdAt = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
 
     @Column(nullable = false)
     private ZonedDateTime updatedAt = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
 
 }
