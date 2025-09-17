cff1ef54a1377b4535a4659e3999d00fa4399a85
chaniii999 <psc0729@naver.com>
2025-09-17 11:34:34 +0900

    feat: add UserMapper for mapping UserSignUpReq to User entity

---
+++ src/main/java/com/calendar/mapper/UserMapper.java
@@ -0,0 +1,20 @@
+package com.calendar.mapper;
+
+import com.calendar.dto.user.UserSignUpReq;
+import com.calendar.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    UserMapper INSTANCE = org.mapstruct.factory.Mappers.getMapper(UserMapper.class);
+
+    @Mapping(target = "id", ignore = true) // ULID는 @PrePersist에서 자동 생성
+    @Mapping(target = "email", source = "dto.email")
+    @Mapping(target = "nickname", source = "dto.nickname")
+    @Mapping(target = "password", ignore = true) // Service 레벨에서 암호화 후 주입
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    User toEntity(UserSignUpReq dto);
+}
