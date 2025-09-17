e9f6342d5fa46150f66cdbd36e4bb8db3d1d2ed0
chaniii999 <psc0729@naver.com>
2025-09-17 10:52:40 +0900

    fix: 특정날짜 조회 메서드 매개변수 startDate 변수 String -> LocalDate로 정정

--- src/main/java/com/calendar/repository/ScheduleRepository.java
+++ src/main/java/com/calendar/repository/ScheduleRepository.java
@@ -4,13 +4,14 @@ import com.calendar.entity.Schedule;
 import com.calendar.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, String> {
 
     // 특정날짜 조회 // n일 조회
-    List<Schedule> findByUserAndStartDate(User user, String startDate);
+    List<Schedule> findByUserAndStartDate(User user, LocalDate startDate);
     // 특정기간 조회 // n일 ~ n일 조회
     List<Schedule> findByUserAndStartDateBetween(User user, LocalDateTime startDate, LocalDateTime endDate);
 }
