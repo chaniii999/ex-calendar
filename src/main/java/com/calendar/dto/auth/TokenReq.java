84ec35ce75d0999445e737bb7d35b5d111598129
chaniii999 <psc0729@naver.com>
2025-09-24 09:20:24 +0900

    refactor: TokenReq 클래스에서 이메일 필드 제거 및 코드 정리

--- src/main/java/com/calendar/dto/auth/TokenReq.java
+++ src/main/java/com/calendar/dto/auth/TokenReq.java
@@ -1,14 +1,10 @@
 package com.calendar.dto.auth;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class TokenReq {
-	@Email
-	@NotBlank
-	private String email;
 
 	@NotBlank
 	private String refreshToken;
